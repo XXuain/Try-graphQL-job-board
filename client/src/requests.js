04df328921bdbb4f29a1ae82ad6800d9b75a2893
yellowHuang <hlady1115@gmail.com>
2021-11-27 09:53:23 +0800

    add: loadjob args

--- client/src/requests.js
+++ client/src/requests.js
@@ -23,3 +23,28 @@ export const loadJobs = async () => {
   const responseBody = await response.json();
   return responseBody.data?.jobs || [];
 };
+
+export const loadJob = async (id) => {
+  const response = await fetch(endpointURL, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      query: `query jobs($id: ID!){
+          job(id: $id){
+            id
+            title
+            description
+            company {
+              id
+              name
+              description
+            }
+          }
+        }
+      `,
+      variables: { id },
+    }),
+  });
+  const responseBody = await response.json();
+  return responseBody.data?.job || {};
+};
